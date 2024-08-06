32d467e84bb76ad86c6fe0376ed47c5cd7d84704
Laszlo Korte <me@laszlokorte.de>
2024-08-06 15:16:47 +0200

    adjust production config

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,7 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :renew_collab, RenewCollab.Repo,
+    adapter: Ecto.Adapters.MyXQL
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
