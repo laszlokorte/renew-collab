58c524470c5c15624af9a548d25fe1ed64c4cb05
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-08-06 18:48:12 +0200

    change password config

--- lib/renew_collab_web/controllers/session_controller.ex
+++ lib/renew_collab_web/controllers/session_controller.ex
@@ -2,10 +2,9 @@ defmodule RenewCollabWeb.SessionController do
   use RenewCollabWeb, :controller
 
   action_fallback RenewCollabWeb.FallbackController
-  @password Application.compile_env(:renew_collab, :app_password)
 
   def new(conn, %{"password" => password}) do
-    if password == @password do
+    if Application.fetch_env!(:renew_collab, :app_password) == password do
       render(conn, :show, token: RenewCollabWeb.Token.sign(%{user_id: 42}))
     else
       conn |> put_status(:unauthorized) |> render(:show, %{:error => "Authentication failed"})
