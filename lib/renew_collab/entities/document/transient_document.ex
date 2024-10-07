8aaca9164f07b498546433d066be6e38be2cca0b
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-10-07 22:00:23 +0200

    rename file to be snake_cased

--- lib/renew_collab/entities/document/transient_document.ex
+++ lib/renew_collab/entities/document/transient_document.ex
@@ -1,7 +1,9 @@
 defmodule RenewCollab.Document.TransientDocument do
+  @default_name "Untitled"
+
   defstruct [:content, :parenthoods, :hyperlinks, :bonds]
 
   def update_name(%__MODULE__{content: content} = doc, updater) do
-    %__MODULE__{doc | content: Map.update(content, :name, "Untitled", updater)}
+    %__MODULE__{doc | content: Map.update(content, :name, @default_name, updater)}
   end
 end
