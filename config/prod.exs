6f156db86745839c2ccf7e424f573d311ed3f144
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-10-08 02:55:18 +0200

    add missing adapter config

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,6 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :renew_collab, RenewCollab.Repo, adapter: Ecto.Adapters.SQLite3
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
