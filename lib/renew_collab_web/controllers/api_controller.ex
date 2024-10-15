d45e0f25309e24bea4f2c857dcddd5d8eac3b649
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-10-15 02:51:07 +0200

    remove old login code

--- lib/renew_collab_web/controllers/api_controller.ex
+++ lib/renew_collab_web/controllers/api_controller.ex
@@ -6,19 +6,4 @@ defmodule RenewCollabWeb.ApiController do
   def index(conn, _params) do
     render(conn, :index)
   end
-
-  def auth(conn, %{"email" => email, "password" => password}) do
-    with %RenewCollabAuth.Entites.Account{} = account <-
-           RenewCollabAuth.Auth.get_account_by_email_and_password(
-             email,
-             password
-           ) do
-      render(conn, :auth, %{
-        account: account
-      })
-    else
-      nil ->
-        conn |> put_status(:unauthorized) |> render(:auth_error)
-    end
-  end
 end
