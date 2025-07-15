c8c5c50c1b98994e741148dcfd6961100b207bc6
Laszlo Korte <laszlokorte@users.noreply.github.com>
2025-07-16 01:00:00 +0200

    fix CORS config to be compatible with otp28

--- lib/renew_collab_web/plugs/cors.ex
+++ lib/renew_collab_web/plugs/cors.ex
@@ -1,10 +1,14 @@
 defmodule RenewCollabWeb.Plug.CORS do
   use Corsica.Router,
-    origins: [~r{^https?://.+$}],
+    origins: [{__MODULE__, :valid_origin?, []}],
     allow_credentials: true,
     allow_headers: :all,
     allow_methods: :all,
     max_age: 600
 
   resource("/api/*")
+
+  def valid_origin?(conn, origin) do
+    String.match?(origin, ~r{^https?://.+$})
+  end
 end
