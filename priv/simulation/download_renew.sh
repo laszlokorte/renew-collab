a11f3031c6024ca9ac2f37dc6efba5b0e975333e
Laszlo Korte <email@laszlokorte.de>
2025-10-02 00:36:34 +0200

    upload download script for renew4.2

--- priv/simulation/download_renew.sh
+++ priv/simulation/download_renew.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 mkdir -p ./out
-wget https://www2.informatik.uni-hamburg.de/TGI/renew/4.1/renew4.1base.zip -O ./out/renew4.1base.zip
-unzip out/renew4.1base.zip -d renew41
+wget https://www2.informatik.uni-hamburg.de/TGI/renew/4.2/renew4.2base.zip -O ./out/renew4.2base.zip
+unzip out/renew4.2base.zip -d ./out/renew42
+mv ./out/renew42/*/* ./renew42/
