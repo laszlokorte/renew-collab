f5d5335449d0b7eee2d81fc577ea0b31ac712e06
Laszlo Korte <me@laszlokorte.de>
2025-10-27 18:59:59 +0100

    continue thumbnail implementation

--- lib/renew_collab_web/controllers/thumbnail_svg.ex
+++ lib/renew_collab_web/controllers/thumbnail_svg.ex
@@ -2,4 +2,7 @@ defmodule RenewCollabWeb.ThumbnailSVG do
   use RenewCollabWeb, :html
 
   embed_templates "thumbnail_html/*"
+
+  def background(%{style: %{background_color: col}}), do: col
+  def background(_layer), do: "gray"
 end
