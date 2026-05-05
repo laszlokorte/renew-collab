laszlokorte/renew-collab

commit 0ddc107c41edba458a32dbc923d5ad4112117eda

optimize preloads

lib/renew_collab_proj/queries/project_details.ex
@@ -11,24 +11,21 @@ defmodule RenewCollabProj.Queries.ProjectDetails do
   def multi(%__MODULE__{project_id: project_id}) do
     Ecto.Multi.new()
     |> Ecto.Multi.one(
-      :result,
+      :projects,
       from(
         p in Project,
-        left_join: m in assoc(p, :members),
-        left_join: d in assoc(p, :documents),
-        left_join: sns in assoc(p, :shadow_net_systems),
-        left_join: s in assoc(p, :simulations),
-        left_join: i in assoc(p, :invitations),
-        where: p.id == ^project_id,
-        order_by: [asc: m.inserted_at, asc: d.inserted_at, asc: i.inserted_at, asc: s.inserted_at],
-        preload: [
-          members: m,
-          documents: d,
-          shadow_net_systems: sns,
-          simulations: s,
-          invitations: i
-        ]
+        where: p.id == ^project_id
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members,
+         :ownerships,
+         :documents,
+         :shadow_net_systems,
+         :simulations
+       ])}
+    end)
   end
 end

lib/renew_collab_proj/queries/all_projects.ex
@@ -11,22 +11,20 @@ defmodule RenewCollabProj.Queries.AllProjects do
   def multi(%__MODULE__{}) do
     Ecto.Multi.new()
     |> Ecto.Multi.all(
-      :result,
+      :projects,
       from(p in Project,
-        left_join: m in assoc(p, :members),
-        left_join: o in assoc(p, :ownerships),
-        left_join: sns in assoc(p, :shadow_net_systems),
-        left_join: d in assoc(p, :documents),
-        left_join: s in assoc(p, :simulations),
-        order_by: [desc: :inserted_at],
-        preload: [
-          ownerships: o,
-          members: m,
-          documents: d,
-          shadow_net_systems: sns,
-          simulations: s
-        ]
+        order_by: [desc: :inserted_at]
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members,
+         :ownerships,
+         :documents,
+         :shadow_net_systems,
+         :simulations
+       ])}
+    end)
   end
 end

lib/renew_collab_proj/queries/own_projects.ex
@@ -11,24 +11,22 @@ defmodule RenewCollabProj.Queries.OwnProjects do
   def multi(%__MODULE__{account_id: account_id}) do
     Ecto.Multi.new()
     |> Ecto.Multi.all(
-      :result,
+      :projects,
       from(p in Project,
         join: mem in assoc(p, :members),
         where: mem.account_id == ^account_id,
-        left_join: m in assoc(p, :members),
-        left_join: o in assoc(p, :ownerships),
-        left_join: sns in assoc(p, :shadow_net_systems),
-        left_join: d in assoc(p, :documents),
-        left_join: s in assoc(p, :simulations),
-        order_by: [desc: :inserted_at],
-        preload: [
-          ownerships: o,
-          members: m,
-          documents: d,
-          shadow_net_systems: sns,
-          simulations: s
-        ]
+        order_by: [desc: :inserted_at]
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members,
+         :ownerships,
+         :documents,
+         :shadow_net_systems,
+         :simulations
+       ])}
+    end)
   end
 end

lib/renew_collab_proj/queries/project_with_members.ex
@@ -23,63 +23,71 @@ defmodule RenewCollabProj.Queries.ProjectWithMembers do
   def multi(%__MODULE__{id: {:project, project_id}}) do
     Ecto.Multi.new()
     |> Ecto.Multi.one(
-      :result,
+      :projects,
       from(
         p in Project,
-        left_join: m in assoc(p, :members),
-        where: p.id == ^project_id,
-        preload: [
-          members: m
-        ]
+        where: p.id == ^project_id
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members
+       ])}
+    end)
   end
 
   def multi(%__MODULE__{id: {:document, document_id}}) do
     Ecto.Multi.new()
     |> Ecto.Multi.one(
-      :result,
+      :projects,
       from(
         p in Project,
         inner_join: d in assoc(p, :documents),
-        on: d.document_id == ^document_id,
-        left_join: m in assoc(p, :members),
-        preload: [
-          members: m
-        ]
+        on: d.document_id == ^document_id
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members
+       ])}
+    end)
   end
 
   def multi(%__MODULE__{id: {:simulation, simulation_id}}) do
     Ecto.Multi.new()
     |> Ecto.Multi.one(
-      :result,
+      :projects,
       from(
         p in Project,
         inner_join: s in assoc(p, :simulations),
-        on: s.simulation_id == ^simulation_id,
-        left_join: m in assoc(p, :members),
-        preload: [
-          members: m
-        ]
+        on: s.simulation_id == ^simulation_id
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members
+       ])}
+    end)
   end
 
   def multi(%__MODULE__{id: {:shadow_net_system, shadow_net_system_id}}) do
     Ecto.Multi.new()
     |> Ecto.Multi.one(
-      :result,
+      :projects,
       from(
         p in Project,
         inner_join: s in assoc(p, :shadow_net_systems),
-        on: s.shadow_net_system_id == ^shadow_net_system_id,
-        left_join: m in assoc(p, :members),
-        preload: [
-          members: m
-        ]
+        on: s.shadow_net_system_id == ^shadow_net_system_id
       )
     )
+    |> Ecto.Multi.run(:result, fn repo, %{projects: projects} ->
+      {:ok,
+       repo.preload(projects, [
+         :members
+       ])}
+    end)
   end
 end

commit db9c6ff75b04f0079404f792a8c7d942f7f1555b

fix query regression introduced in 0ddc107c4

lib/renew_collab_proj/queries/project_details.ex
@@ -24,7 +24,8 @@ defmodule RenewCollabProj.Queries.ProjectDetails do
          :ownerships,
          :documents,
          :shadow_net_systems,
-         :simulations
+         :simulations,
+         :invitations
        ])}
     end)
   end